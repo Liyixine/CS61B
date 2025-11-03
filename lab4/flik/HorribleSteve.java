9f692908a48c8c247c2a09fcc50bcc89daec5fbb
Liyi <3756075970@qq.com>
2025-11-03 15:31:37 +0800

    implement the lab4 debugging

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,9 +1,10 @@
 package flik;
 
 public class HorribleSteve {
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        int testTurn = 500
+        for (int j = 0; i < testTurn; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
